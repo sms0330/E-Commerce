2ee11e99f89d32b89f3d396a6c0c4b431ad8831f
Joseph Son <sms0330@gmail.com>
2021-09-16 22:49:12 -0700

    Added Link to shoe's image / title

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 const ProductShow = ({shoes}) => {
@@ -8,8 +9,8 @@ const ProductShow = ({shoes}) => {
       <div className="row">
         {shoes.map((shoe, index) => (
           <div className="col-md-4" key={index}>
-            <img src={`https://codingapple1.github.io/shop/shoes${index+1}.jpg`} width="100%" alt="shoe" />
-            <h4>{shoe.title}</h4>
+            <Link to={`/detail/${shoe.id}`}><img src={`https://codingapple1.github.io/shop/shoes${index+1}.jpg`} width="100%" alt="shoe" /></Link>
+            <h4><Link to={`/detail/${shoe.id}`}>{shoe.title}</Link></h4>
             <p>{shoe.content}</p>
             <p>${shoe.price}</p>
           </div>
