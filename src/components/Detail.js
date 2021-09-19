7b32aa4cf416c16e909fbd62487a4c6557a456c4
sms0330 <sms0330@gmail.com>
2021-09-18 17:09:26 -0700

    Added styled-components, node-sass, useEffect hooks

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,25 +1,95 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import './Detail.scss';
 
-const Detail = ({shoes}) => {
+const Box = styled.div`
+  padding: 20px;
+`;
+
+const Title = styled.h4`
+  font-size: 25px;
+  color: ${props => props.color};
+`;
+
+const Detail = ({ shoes }) => {
+  const [alert, setAlert] = useState(true);
+  const [alert2, setAlert2] = useState(false);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [alert]);
+
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      setAlert2(true);
+    }, 5000);
+    timer = setTimeout(() => {
+      setAlert2(false);
+    }, 7000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [alert]);
 
   const { id } = useParams();
-  const history = useHistory()
-  const findShoes = shoes.find(result => result.id == id)
-  console.log(findShoes)
+  const history = useHistory();
+  const findShoes = shoes.find(result => result.id == id);
+  console.log(findShoes);
 
   return (
     <div className="container">
+      <Box>
+        <Title className="red">Detail Page</Title>
+        <Title color="blue">Detail Page</Title>
+      </Box>
+      {data}
+      <input
+        onChange={e => {
+          setData(e.target.value);
+        }}
+      />
+
+      {alert === true ? (
+        <div className="my-alert-yellow">
+          <p>Stock is almost running out.</p>
+        </div>
+      ) : null}
+      {alert2 === true ? (
+        <div className="my-alert-red">
+          <p>Stock is almost running out.</p>
+        </div>
+      ) : null}
+
       <div className="row">
         <div className="col-md-6">
-          <img src={`https://codingapple1.github.io/shop/shoes${findShoes.id+1}.jpg`} width="100%" alt="shoe" />
+          <img
+            src={`https://codingapple1.github.io/shop/shoes${
+              findShoes.id + 1
+            }.jpg`}
+            width="100%"
+            alt="shoe"
+          />
         </div>
         <div className="col-md-6 mt-4">
           <h4 className="pt-5">{findShoes.title}</h4>
           <p>{findShoes.content}</p>
           <p>{findShoes.price}</p>
           <button className="btn btn-danger">Order</button>
-          <button className="btn btn-primary" onClick={()=>{history.goBack();}}>Back</button>
+          <button
+            className="btn btn-primary"
+            onClick={() => {
+              history.goBack();
+            }}
+          >
+            Back
+          </button>
         </div>
       </div>
     </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,14 +10,22 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link><Link to="/">Home</Link></Nav.Link>
-            <Nav.Link><Link to="/detail">Detail</Link></Nav.Link>
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/detail">
+              Detail
+            </Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
